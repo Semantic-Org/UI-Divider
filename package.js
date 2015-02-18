d2b40ec7ee794b5aa144a77a8d98c8ea60fa8fff
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:24:50 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-divider',
+  summary : 'Semantic UI - Divider (official): Single component release of divider',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Divider.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
