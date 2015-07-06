d8ac0500ac1d668f75c49ce708098f386bbe0fa8
Pusher Robot <jack@semantic-ui.com>
2015-07-06 17:02:26 -0400

    Updated component to version 2.0.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-divider',
   summary : 'Semantic UI - Divider: Single component release',
-  version : '2.0.0',
+  version : '2.0.1',
   git     : 'git://github.com/Semantic-Org/UI-Divider.git',
 });
 
