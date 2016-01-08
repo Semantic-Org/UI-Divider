5656747570bae390d4a8b01f9dfad082e5a40ec6
Jack Lukic <jack@semantic-ui.com>
2016-01-07 23:53:12 -0500

    Updated component to version 2.1.8

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-divider',
   summary : 'Semantic UI - Divider: Single component release',
-  version : '2.1.7',
+  version : '2.1.8',
   git     : 'git://github.com/Semantic-Org/UI-Divider.git',
 });
 
